eefff2a70ebfea5625a96ac4e0b2a4d1a55b1817
BIANCA LO <30097639@tafe.wa.edu.au>
2026-02-13 22:57:12 +0800

    - added Galileo6.dll as project reference - added LinkedList for Sensor A and B - made LoadData method that generates data for sensor A and B - added numerical input control for standard dev and mean - app now loads data to listview and listboxes

--- MSSS-SatelliteDataProcessing/MainWindow.xaml.cs
+++ MSSS-SatelliteDataProcessing/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Galileo6;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MSSS_SatelliteDataProcessing
 {
@@ -16,13 +18,43 @@ namespace MSSS_SatelliteDataProcessing
     /// </summary>
     public partial class MainWindow : Window
     {
+        LinkedList<double> SensorA { get; } = new LinkedList<double>();
+        LinkedList<double> SensorB { get; } = new LinkedList<double>();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void LoadData()
         {
+            ReadData data = new ReadData();
+
+            double sigma = double.Parse(txtSigma.Text);
+            double mu = double.Parse(txtMean.Text);
+
+            for (int i = 0; i <= 400; i++)
+            {
+                SensorA.AddLast(data.SensorA(mu, sigma));
+                SensorB.AddLast(data.SensorB(mu, sigma));
+            }
+        }
+
+        private void btnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+
+            for(int i = 0; i < SensorA.Count && i< SensorB.Count; i++)
+            {
+                lstSensorA.Items.Add(SensorA.ElementAt(i));
+                lstSensorB.Items.Add(SensorB.ElementAt(i));
+
+                lvSensorData.Items.Add(new
+                {
+                    SensorA = SensorA.ElementAt(i),
+                    SensorB = SensorB.ElementAt(i)
+                });
+            }
 
         }
     }
